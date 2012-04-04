243c44147650956884b9b7d9d7793c9095668928
Hakuna Matata <hakunamatata@Hakunas-Mac-mini.local>
2012-04-04 22:29:42 +0530

    first commit

---
+++ sam/sam/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  sam
+//
+//  Created by Hakuna Matata on 04/04/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ sam/sam/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  sam
+//
+//  Created by Hakuna Matata on 04/04/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
